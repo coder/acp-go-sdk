aba87cd94b2d9c883c8abdd28660849ad4842bdd
Thomas Kosiewski <tk@coder.com>
2026-02-19 21:23:17 +0100

    Assert unstable dispatch capabilities per method
    Change-Id: I0bab8ffb5e201d1c82d5fd9aba087fa45f995787
    Signed-off-by: Thomas Kosiewski <tk@coder.com>

--- cmd/generate/internal/emit/jenwrap.go
+++ cmd/generate/internal/emit/jenwrap.go
@@ -11,30 +11,31 @@ type (
 )
 
 var (
-	NewFile = jen.NewFile
-	Id      = jen.Id
-	Lit     = jen.Lit
-	Line    = jen.Line
-	Func    = jen.Func
-	For     = jen.For
-	Range   = jen.Range
-	Return  = jen.Return
-	Nil     = jen.Nil
-	String  = jen.String
-	Int     = jen.Int
-	Float64 = jen.Float64
-	Bool    = jen.Bool
-	Any     = jen.Any
-	Map     = jen.Map
-	Index   = jen.Index
-	Qual    = jen.Qual
-	Error   = jen.Error
-	Case    = jen.Case
-	Default = jen.Default
-	Switch  = jen.Switch
-	Var     = jen.Var
-	If      = jen.If
-	List    = jen.List
-	Op      = jen.Op
-	Comment = jen.Comment
+	NewFile       = jen.NewFile
+	Id            = jen.Id
+	Lit           = jen.Lit
+	Line          = jen.Line
+	Func          = jen.Func
+	For           = jen.For
+	Range         = jen.Range
+	Return        = jen.Return
+	Nil           = jen.Nil
+	String        = jen.String
+	Int           = jen.Int
+	Float64       = jen.Float64
+	Bool          = jen.Bool
+	Any           = jen.Any
+	Map           = jen.Map
+	Index         = jen.Index
+	Qual          = jen.Qual
+	Error         = jen.Error
+	Case          = jen.Case
+	Default       = jen.Default
+	Switch        = jen.Switch
+	Var           = jen.Var
+	If            = jen.If
+	List          = jen.List
+	Op            = jen.Op
+	InterfaceFunc = jen.InterfaceFunc
+	Comment       = jen.Comment
 )
